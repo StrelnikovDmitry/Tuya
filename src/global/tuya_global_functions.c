6e4f30368d94cb87e8894065a7d60cc61bc5fe88
StrelnikovDmitry <Formeazaz@gmail.com>
2026-08-19 23:51:54 +0700

    add get_buffer_size function
    get_buffer_size function gets the terminal size and multiplies it by a
    float given
    
    TODO: make tuya_shutdown function and signal hadler for SIGINT and
    SIGWINCH

--- src/global/tuya_global_functions.c
+++ src/global/tuya_global_functions.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 // MUST BE the first command in the code
 // buffer size value can be get from the special function get_buffer_size
@@ -10,4 +13,22 @@ void tuya_init(size_t buffer_size, unsigned char should_hide_cursor) {
         printf("\033[?25l");
     }
     fflush(stdout);
+}
+
+// If you want to update the whole screen every frame, you should use multiplicator 4 or more.
+// Either way, 0.5 or 1 is usually enough.
+size_t get_buffer_size(float multiply_by) {
+    struct winsize ws;
+    int result = ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+    if (result != -1) {
+        size_t buffer_size = ws.ws_col*ws.ws_row*multiply_by;
+        return buffer_size;
+    }
+    else {
+        fputs("TUYA ERROR: NO TERMINAL SIZE\nThis usually means stdout is not connected to a terminal, or your OS isn't supported yet.\n\n Check official tuya github: https://github.com/StrelnikovDmitry/tuya/issues or write your own issue.", stderr);
+        // TEMPORARY WAY OF CRASHING PROGRAMM: it will change after tuya_shutdown function will be created
+        abort();
+        // TODO:
+        // make normal tuya_shutdown function
+    }
 }
