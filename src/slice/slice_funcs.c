50511f9905e23808951bb5aa53b899e7b432a781
StrelnikovDmitryD <formeazaz@gmail.com>
2026-08-21 22:53:51 +0700

    update size value in create_slice function

--- src/slice/slice_funcs.c
+++ src/slice/slice_funcs.c
@@ -1,11 +1,12 @@
 #include "slice_structure.h"
 #include <stdlib.h>
 
-Slice create_slice(int x1, int y1, int x2, int y2, char *content) {
-	Slice slice;
+Slice create_slice(int x1, int y1, int x2, int y2) {
+    Slice slice;
 
-	slice.size = sizeof(char)*(x2-x1)*(y2-y1)+1;
+	slice.size = sizeof(char) * ((x2 - (x1 - 1)) + 1) * (y2 - (y1 - 1));
 	slice.pointer = malloc(slice.size);
+	slice.pointer[slice.size-1] = '\0';
 
 	slice.x1 = x1;
 	slice.y1 = y1;
