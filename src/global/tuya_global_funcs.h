a7add0deee4adb67b3ae152f765527b6e1e98700
StrelnikovDmitryD <formeazaz@gmail.com>
2026-08-21 23:05:28 +0700

    create tuya_global_funcs.h

---
+++ src/global/tuya_global_funcs.h
@@ -0,0 +1,10 @@
+#ifndef GLOBAL_FUNCS
+#define GLOBAL_FUNCS
+
+#include <stdlib.h>
+
+void tuya_init(size_t buffer_size, unsigned char should_hide_cursor);
+void tuya_shutdown();
+size_t get_buffer_size(float multiply_by);
+
+#endif
