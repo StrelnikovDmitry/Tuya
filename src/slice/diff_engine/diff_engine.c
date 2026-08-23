59a00ba67b565b869321480a2361ceb6fd706057
StrelnikovDmitryD <formeazaz@gmail.com>
2026-08-23 21:58:28 +0700

    create diff_engine.c and get_length function inside
    create diff_engine.c in src/slice/diff_engine
    implement binary search aalgorithm for finding length of an int given. It will be used for future "moving_cursor_cost" function.

---
+++ src/slice/diff_engine/diff_engine.c
@@ -0,0 +1,48 @@
+int get_length(int n) {
+	if (n < 100000) {
+		if (n < 1000) {
+			if (n < 100) {
+				if (n < 10) {
+					return 1;
+				} 
+				else {
+					return 2;
+				}
+			}
+			else {
+				return 3;
+			}
+		}
+		else {
+			if (n < 10000) {
+				return 4;
+			}
+			else {
+				return 5;
+			}
+		}
+	}
+	else {
+		if (n < 100000000) {
+			if (n < 10000000) {
+				if (n < 1000000) {
+					return 6;
+				}
+				else {
+					return 7;
+				}
+			}
+			else {
+				return 8;
+			}
+		}
+		else {
+			if (n < 1000000000) {
+				return 9;
+			}
+			else {
+				return 10;
+			}
+		}
+	}
+}
