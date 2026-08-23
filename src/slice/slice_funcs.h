7bfac0f93b9b4dd87cbd3d4377aba10e9af461c8
StrelnikovDmitryD <formeazaz@gmail.com>
2026-08-23 23:59:30 +0700

    update slice_funcs.h
    update slice_funcs.h with get_honest_width, get_width, get_height.

--- src/slice/slice_funcs.h
+++ src/slice/slice_funcs.h
@@ -5,5 +5,8 @@
 
 void slice_update_force(Slice sl, char *content);
 Slice create_slice(int x1, int y1, int x2, int y2);
+int get_honest_width(Slice *sl);
+int get_width(Slice *sl);
+int get_height(Slice *sl);
 
 #endif
