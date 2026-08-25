536e394f48d99003f91cbbc1bece725aa49698a4
StrelnikovDmitryD <formeazaz@gmail.com>
2026-08-25 16:31:57 +0700

    delete unworking fuction and add includes needed

--- src/slice/diff_engine/diff_engine.c
+++ src/slice/diff_engine/diff_engine.c
@@ -1,6 +1,7 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include "../slice_funcs.h"
 #include "../slice_structure.h"
+#include "../../cursor/cursor_funcs.h"
 
 int get_length(int n) {
 	int length = 1;
@@ -18,38 +19,5 @@ int get_length(int n) {
 }
 
 int moving_cursor_cost(int x, int y) {
-	return 4+get_length(x)+get_length(y);	
-}
-
-void update(Slice* sl, char* string) {
-	int i = 0;
-	int start_edit = 0;
-	int end_edit = 0;
-	unsigned char edit = 0;
-	while (sl->pointer[i] != '\0') {
-		if (string[i] != sl->pointer[i]) {
-			if (!edit) {
-				start_edit = i;
-				edit = 1;
-			}
-			end_edit = i;
-			sl->pointer[i] = string[i];
-		}
-		if (edit) {
-			edit = 0;
-			int x = start_edit % get_width(sl);
-			int y = start_edit / get_width(sl);
-			printf("\033[%d;%dH", x, y);
-			for (int j = start_edit; j <= end_edit; j++) {
-				x++;
-				if (x > get_width(sl)) {
-					x = sl->x1;
-					y++;
-				}
-				printf("%c", string[j]);
-			}
-			fflush(stdout);
-		}
-		i++;
-	}
+	return 4+get_length(x)+get_length(y);
 }
