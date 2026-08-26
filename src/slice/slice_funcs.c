c27eceff92359cfc278e28e031c0289cbfa890dd
StrelnikovDmitry <formeazaz@gmail.com>
2026-08-26 21:39:11 +0700

    fix comments in slice_funcs.c

--- src/slice/slice_funcs.c
+++ src/slice/slice_funcs.c
@@ -6,10 +6,10 @@
 Slice create_slice(int x1, int y1, int x2, int y2) {
     Slice slice;
 
-    // to store '\0' and '\n', width should bee bigger by one.
+    // to store '\0' and '\n', width should be bigger by one.
 	slice.size = sizeof(char) * ((x2 - (x1 - 1)) + 1) * (y2 - (y1 - 1));
 	slice.pointer = malloc(slice.size);
-	slice.pointer[slice.size-1] = '\0';
+	slice.pointer[slice.size - 1] = '\0';
 
 	slice.x1 = x1;
 	slice.y1 = y1;
