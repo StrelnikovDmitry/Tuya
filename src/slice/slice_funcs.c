944ee32bb301556febd243092393bd363c5d3710
StrelnikovDmitry <formeazaz@gmail.com>
2026-08-28 20:28:54 +0700

    rename count_lines to lines_count in update force and assign it to 0 by default

--- src/slice/slice_funcs.c
+++ src/slice/slice_funcs.c
@@ -38,33 +38,33 @@ void slice_update_force(Slice *sl, char *content) {
     int width = get_width(sl);
     int height = sl->y2 - (sl->y1 - 1);
 
-    int count_lines = 1;
+    int lines_count = 0;
 
     for (int i = 0; i < sl->size; i++) {
         // if new state ended, end the content in slice
-        if (content[i - (count_lines - 1)] == '\0') {
+        if (content[i - lines_count] == '\0') {
             sl->pointer[i] = '\0';
             break;
         }
         else {
             //if there shouldn't start another line
             if ((i + 1) % width) {
-                sl->pointer[i] = content[i - (count_lines - 1)];
+                sl->pointer[i] = content[i - lines_count];
             }
             //if there should
             else {
                 // if there can't fit new line
-                if (count_lines + 1 > height) {
+                if (lines_count + 2 > height) {
                     sl->pointer[i] = '\0';
                     break;
                 }
                 // if there can
                 else {
                     sl->pointer[i] = '\n';
-                    move_cursor(sl->x1, count_lines - 1 + sl->y1);
-                    count_lines++;
+                    move_cursor(sl->x1, lines_count + sl->y1);
+                    lines_count++;
                     i++;
-                    sl->pointer[i] = content[i - (count_lines - 1)];
+                    sl->pointer[i] = content[i - lines_count];
                 }
             }
         }
