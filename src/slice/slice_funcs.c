ef0ee239fdba5bbdd8cdb072c42a8f47b6991fb1
StrelnikovDmitry <formeazaz@gmail.com>
2026-08-30 20:05:47 +0700

    make force update function compatible with new compact buffer

--- src/slice/slice_funcs.c
+++ src/slice/slice_funcs.c
@@ -38,33 +38,30 @@ void FORCE_slice_update(Slice *sl, char *content) {
     int width = get_width(sl);
     int height = sl->y2 - (sl->y1 - 1);
 
-    int lines_count = 0;
+    int y = sl->y1;
 
     for (int i = 0; i < sl->size; i++) {
         // if new state ended, end the content in slice
-        if (content[i - lines_count] == '\0') {
+        if (content[i] == '\0') {
             sl->pointer[i] = '\0';
             break;
         }
         else {
             //if there shouldn't start another line
             if ((i + 1) % width) {
-                sl->pointer[i] = content[i - lines_count];
+                sl->pointer[i] = content[i];
             }
             //if there should
             else {
                 // if there can't fit new line
-                if (lines_count + 2 > height) {
-                    sl->pointer[i] = '\0';
+                if (y + 1 > sl->y1 + height) {
                     break;
                 }
                 // if there can
                 else {
-                    sl->pointer[i] = '\n';
-                    move_cursor(sl->x1, lines_count + sl->y1);
-                    lines_count++;
-                    i++;
-                    sl->pointer[i] = content[i - lines_count];
+                    sl->pointer[i] = content[i];
+                    y++;
+                    move_cursor(sl->x1, y);
                 }
             }
         }
