37abf149b5072a1ad65f65d24a93243a808eb151
StrelnikovDmitry <formeazaz@gmail.com>
2026-08-31 21:37:41 +0700

    add comments in diff_engine.c

--- src/slice/diff_engine/diff_engine.c
+++ src/slice/diff_engine/diff_engine.c
@@ -3,6 +3,8 @@
 #include "../slice_funcs.h"
 #include "../../cursor/cursor_funcs.h"
 
+// branchless algorithm for findin length of an integer given
+// it could be binary search algorithm but this one is faster practically
 int get_length(int n) {
 	int length = 1;
 	length += (n > 9);
@@ -18,10 +20,12 @@ int get_length(int n) {
 	return length;
 }
 
+// getting cost of ANSI code moving cursor to the given place
 int moving_cursor_cost(int x, int y) {
 	return 4+get_length(x)+get_length(y);
 }
 
+// updating a small part of slice given.
 void update_sequence(int start_x, int start_y, int start_edit, int end_edit, Slice *sl, char *content) {
 	move_cursor(start_x, start_y);
 	for (int c = start_edit - 1; c <= end_edit - 1; c++) {
@@ -33,9 +37,14 @@ void update_sequence(int start_x, int start_y, int start_edit, int end_edit, Sli
 void update_slice(Slice *sl, char *content) {
     int x = sl->x1;
     int y = sl->y1;
+
+    // the buffer will be between indexes (start_edit - 1) and end (edit - 1)
+    // assigning it to zero so some if statements that are designed to react only when the
+    // start edit is assigned to a value didn't react
     int start_edit = 0;
     int end_edit = 0;
 
+	// coordinates of a (start_edit - 1) index
 	int start_x = 0;
 	int start_y = 0;
 
@@ -59,6 +68,10 @@ void update_slice(Slice *sl, char *content) {
             end_edit = i + 1;
         }
         else {
+        	// compare the amount of unchanged chars we need to change if we don't move cursor
+        	// with the cost of cursor moving
+        	// basically, if the cost is higher than the amount of unchanged symbols,
+        	// we change the symbols beetwen to buffers to reduce CPU usage (printf usage)
         	if (start_edit && moving_cursor_cost(start_x, start_y) < i - end_edit) {
         		update_sequence(start_x, start_y, start_edit, end_edit, sl, content);
         	}
@@ -66,9 +79,11 @@ void update_slice(Slice *sl, char *content) {
 
         x++;
     }
+    // final update if needed
     if (start_edit) {
         update_sequence(start_x, start_y, start_edit, end_edit, sl, content);
         start_edit = 0;
     }
+    // fflushing
     fflush(stdout);
 }
