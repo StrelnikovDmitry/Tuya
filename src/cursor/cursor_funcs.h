6e982e8b84a9bd776cd8df9794a55cfbcb9fc636
StrelnikovDmitry <formeazaz@gmail.com>
2026-09-02 20:01:33 +0700

    update ansi utility header

--- src/cursor/cursor_funcs.h
+++ src/cursor/cursor_funcs.h
@@ -2,9 +2,13 @@
 #define CURSOR
 
 void move_cursor(int x, int y);
+
 void clear_the_rest();
+void clear_all();
+
 void show_cursor();
 void hide_cursor();
+
 void turn_alt_screen_on();
 void turn_alt_screen_off();
 
