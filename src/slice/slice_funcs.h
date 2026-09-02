9ddb641a666542b1f8b3f34bc8b0d0479f8e4e25
StrelnikovDmitry <formeazaz@gmail.com>
2026-09-02 18:59:05 +0700

    update header file for slice functions

--- src/slice/slice_funcs.h
+++ src/slice/slice_funcs.h
@@ -3,10 +3,13 @@
 
 #include "slice_structure.h"
 
-void FORCE_update_slice(Slice *sl, char *content);
 Slice create_slice(int x1, int y1, int x2, int y2);
+void delete_slice(Slice *sl);
+
 int get_honest_width(Slice *sl);
 int get_width(Slice *sl);
 int get_height(Slice *sl);
+
+void FORCE_update_slice(Slice *sl, char *content);
 
 #endif
