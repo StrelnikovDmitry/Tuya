36e3dede0c5322bf6ffb808b6e9e069130b20fa4
StrelnikovDmitry <formeazaz@gmail.com>
2026-09-02 15:44:39 +0700

    update tuya_global_funcs header file
    add to a header to new functions for getting width and height of a
    terminal

--- src/global/tuya_global_funcs.h
+++ src/global/tuya_global_funcs.h
@@ -5,6 +5,8 @@
 
 void tuya_init(size_t buffer_size, unsigned char should_hide_cursor);
 void tuya_shutdown();
+int get_terminal_height();
+int get_terminal_width();
 size_t get_buffer_size(float multiply_by);
 
 #endif
