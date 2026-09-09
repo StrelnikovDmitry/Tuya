c0810fc2e1e76f0ac5c7ccda875ad7f691830d50
StrelnikovDmitry <formeazaz@gmail.com>
2026-09-09 21:30:58 +0700

    create a public api header containing experimental functions

---
+++ include/tuya_raw.h
@@ -0,0 +1,11 @@
+/* This file is an open api header
+ * Functions provided are experimental and subjected to change
+ * I do not guarantee future support of these functions */
+
+#ifndef RAW
+#define RAW
+
+// ansi codes
+#include "../src/terminal/ansi.h"
+
+#endif
