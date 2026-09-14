b9e9afc3e5f5c14a6939765e6b6a36057b89be3e
StrelnikovDmitry <formeazaz@gmail.com>
2026-09-14 15:54:18 +0700

    add new funcs to the tuya_raw API

--- include/tuya_raw.h
+++ include/tuya_raw.h
@@ -19,7 +19,14 @@ void hide_cursor();
 void turn_alt_screen_on();
 void turn_alt_screen_off();
 
+// raw input mode
+void enable_raw(struct termios *orig_termios);
+void disable_raw(struct termios *orig_termios);
+
 // no-diff update
 void FORCE_update_slice(Slice *sl, char *content);
+
+// --- WIDGETS ---
+char* ask_input(int x1, int x2, int y, char *message);
 
 #endif
